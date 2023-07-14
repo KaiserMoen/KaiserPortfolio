4243f10425df39b42d281197a83fbb68c96d9503
Kaiser <nnhelg@gmail.com>
2023-07-14 17:45:02 +0800

    Added Home Designs

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -3,18 +3,26 @@ import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import '../styles/Home.css';
 import { motion,useInView,useAnimation } from 'framer-motion';
-import  prof  from '/public/Prof_design.png';
+import  prof  from '/public/prof_edited.jpg';
 function Home(){
     
     return(
         <div id= 'home_cont'>
             <div className='descriptions'>
-                <h1>LOREMIPSUMLORE<br></br>MIPSUMLOREMIPSU<br></br>MLOREMIPSUM</h1>
-                <h3>loremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsumloremipsum</h3>
-                <p>Continue</p>
+                <div className='vert'>
+                    PROGRAM | DESIGN | 
+                </div>
+                <div>
+                    <h1><span className='dev_text'>DEVELOPER</span> . DESIGNER<br></br>UX/UI . <span className='web_text'>WEBSITE</span><br></br>DEVELOPER . <span className='UX_text'>UX/UI DESIGNER</span></h1>
+                    {/* <h3>loremipsumloremipsumloremipsumloremipsumloremipsum loremipsumloremipsumloremipsumloremipsumloremipsum</h3> */}
+                    <p>Continue</p>
+                </div>
+    
+                
             </div>
+
             <div className='image_cont_prof'>
-                <Image src={prof}></Image>
+                <Image src={prof} className='image'></Image>
             </div>
             
         </div>  
