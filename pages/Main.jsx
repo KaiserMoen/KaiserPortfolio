6e6722dccffd2a4490d9e6096ccfbf74aa659983
Kaiser <nnhelg@gmail.com>
2023-08-19 00:40:51 +0800

    Fixed Error

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -2,7 +2,6 @@ import Home from './Home.jsx'
 import About from './About.jsx'
 import Projects from './Projects.jsx'
 import NavBar from './NavBar.jsx'
-import '../styles/Main.css';
 function Main(){
     return(
         <div className='main'>
