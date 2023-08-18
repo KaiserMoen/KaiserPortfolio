32aa60e12c51bba5473f9cf44e68501784f5448c
Kaiser <nnhelg@gmail.com>
2023-08-18 23:18:48 +0800

    Router .5

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,11 @@
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Projects from './Projects.jsx'
+import NavBar from './NavBar.jsx'
 function Main(){
     return(
         <div>
+
             <Home></Home>
             <About></About>
             <Projects></Projects>
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 'use client'
-import NavBar from './components/NavBar.jsx'
-import Home from './components/Home.jsx'
-import About from './components/About.jsx'
-import Projects from './components/Projects.jsx'
-import Main from './components/Main.jsx'
-import { createBrowserRouter, Routes,Route, Link, createRoutesFromElements, RouterProvider, Outlet } from 'react-router-dom';
+import NavBar from '../../components/NavBar.jsx'
+import Home from '../../components/Home.jsx'
+import About from '../../components/About.jsx'
+import Projects from '../../components/Projects.jsx'
+import Main from '../../components/Main.jsx'
+import { createBrowserRouter,Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 function App(){
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path = "/" element={<NavBar/>} >
-        <Route index element = {<Home/>}/>
+        <Route index element = {<Main/>}/>
         <Route path = "about" element = {<About/>}/>
         <Route path='project' element = {<Projects/>}/>
       </Route>
@@ -18,8 +18,8 @@ function App(){
   return(
     <div className='main'>
       <RouterProvider router = {router}></RouterProvider>
+      <Main></Main>
     </div>
-    
   );
 }
 
